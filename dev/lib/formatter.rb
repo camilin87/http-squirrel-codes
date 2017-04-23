21c061c6c5865a0ac3dfc904ea78e4ee868eeb0b
Camilo Sanchez <camilin87@gmail.com>
2017-04-23 01:37:06 -0400

    create a formatter to generate the templates

---
+++ dev/lib/formatter.rb
@@ -0,0 +1,13 @@
+class Formatter
+    def self.format(status_code_info)
+        %{
+---
+type: page
+title: #{status_code_info.full_name}
+permalink: /#{status_code_info.code}/
+---
+
+<img src="#{status_code_info.url}" alt="#{status_code_info.full_name}" />
+}
+    end
+end
---
+++ dev/spec/formatter_spec.rb
@@ -0,0 +1,21 @@
+require "status_code_info"
+require "formatter"
+
+describe Formatter do
+    it "properly formats the status code info" do
+        input = StatusCodeInfo.new(725, "It works on my machine", "http://tddapps.com/logo.png")
+        expected = %{
+---
+type: page
+title: 725 - It works on my machine
+permalink: /725/
+---
+
+<img src="http://tddapps.com/logo.png" alt="725 - It works on my machine" />
+}
+
+        output = Formatter.format(input)
+
+        expect(output).to eq expected
+    end
+end
