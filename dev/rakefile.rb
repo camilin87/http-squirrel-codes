f36cabf0bb8cd3bf1a3cf4c1eb53e7a53b85c1f6
Camilo Sanchez <camilin87@gmail.com>
2017-04-23 12:50:04 -0400

    ruby’s temp dir is the best

--- dev/rakefile.rb
+++ dev/rakefile.rb
@@ -1,3 +1,5 @@
+require "tmpdir"
+
 task :t => [:test_unit, :test_integration]
 
 task :test_unit do
@@ -6,4 +8,13 @@ end
 
 task :test_integration do
     puts "running integration tests"
+    temp_dir = ""
+
+    Dir.mktmpdir {|dir|
+        temp_dir = dir
+        puts "Temp dir: #{temp_dir}"
+
+        puts "Temp Directory Exists #{File.directory?(temp_dir)}"
+    }
+    puts "Temp Directory Exists #{File.directory?(temp_dir)}"
 end
