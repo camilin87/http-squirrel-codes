b7d0de5a85e30cf57fb600d4582b345030fe1a80
Camilo Sanchez <camilin87@gmail.com>
2017-04-23 00:57:36 -0400

    double quotes all the things

--- dev/spec/parser_spec.rb
+++ dev/spec/parser_spec.rb
@@ -1,22 +1,22 @@
-require 'parser'
+require "parser"
 
 describe Parser do
     def parse(input)
         Parser.parse input
     end
 
-    it 'returns empty on invalid inputs' do
+    it "returns empty on invalid inputs" do
         expect(parse nil).to eq []
         expect(parse "").to eq []
         expect(parse " ").to eq []
     end
 
-    it 'returns empty on incorrect inputs' do
+    it "returns empty on incorrect inputs" do
         expect(parse "invalid").to eq []
         expect(parse "###100 - Continue").to eq []
     end
 
-    it 'builds one image definition per valid line' do
+    it "builds one image definition per valid line" do
         input = "![100 - Continue](http://i.imgur.com/mi9lcO6.jpg)  "
 
         info = parse(input)[0]
