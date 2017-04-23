64e3a1dfed14cc3428ee65f3b91dadb5c8590389
Camilo Sanchez <camilin87@gmail.com>
2017-04-23 00:07:45 -0400

    start building up the parser

--- dev/lib/parser.rb
+++ dev/lib/parser.rb
@@ -1,5 +1,22 @@
+class StatusCodeInfo
+
+end
+
 class Parser
     def self.parse(markdown)
-        []
+        markdown = markdown || ""
+        markdown = markdown.strip
+
+        if markdown == ""
+            return []
+        end
+
+        if not markdown.start_with? "!"
+            return []
+        end
+
+        [
+            StatusCodeInfo.new()
+        ]
     end
 end
--- dev/spec/parser_spec.rb
+++ dev/spec/parser_spec.rb
@@ -1,7 +1,23 @@
 require 'parser'
 
 describe Parser do
-    it 'returns empty by default' do
-        expect(Parser.parse "").to eq []
+    def parse(input)
+        Parser.parse input
+    end
+
+    it 'returns empty on invalid inputs' do
+        expect(parse nil).to eq []
+        expect(parse "").to eq []
+        expect(parse " ").to eq []
+    end
+
+    it 'returns empty on incorrect inputs' do
+        expect(parse "invalid").to eq []
+        expect(parse "###100 - Continue").to eq []
+    end
+
+    it 'builds one image definition per valid line' do
+        input = "![100 - Continue](http://i.imgur.com/mi9lcO6.jpg)  "
+        expect(parse(input)[0]).to be_kind_of(StatusCodeInfo)
     end
 end
