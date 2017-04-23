8f3b871ee8730a04df8ebcae8e973173f6395fde
Camilo Sanchez <camilin87@gmail.com>
2017-04-23 12:51:56 -0400

    remove the temp directory validation

--- dev/rakefile.rb
+++ dev/rakefile.rb
@@ -8,13 +8,8 @@ end
 
 task :test_integration do
     puts "running integration tests"
-    temp_dir = ""
 
-    Dir.mktmpdir {|dir|
-        temp_dir = dir
-        puts "Temp dir: #{temp_dir}"
-
-        puts "Temp Directory Exists #{File.directory?(temp_dir)}"
+    Dir.mktmpdir {|temp_dir|
+        puts "Temp Dir: #{temp_dir}"
     }
-    puts "Temp Directory Exists #{File.directory?(temp_dir)}"
 end
