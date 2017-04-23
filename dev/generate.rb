087d4c488aca88cbc69874ad11c6818d7396850c
Camilo Sanchez <camilin87@gmail.com>
2017-04-23 13:16:46 -0400

    start the website generation

---
+++ dev/generate.rb
@@ -0,0 +1,11 @@
+require_relative "lib/formatter"
+require_relative "lib/parser"
+
+puts "Generating Website..."
+
+input_file = ARGV[0]
+output_dir = ARGV[1]
+puts "input_file: #{input_file}"
+puts "output_dir: #{output_dir}"
+
+
