bf5d01fad441de2c34ab546dbaaa03e9733ddbaf
Camilo Sanchez <camilin87@gmail.com>
2017-04-23 13:23:56 -0400

    implement the generation

--- dev/generate.rb
+++ dev/generate.rb
@@ -1,5 +1,6 @@
 require_relative "lib/formatter"
 require_relative "lib/parser"
+require_relative "lib/status_code_info"
 
 puts "Generating Website..."
 
@@ -8,4 +9,16 @@ output_dir = ARGV[1]
 puts "input_file: #{input_file}"
 puts "output_dir: #{output_dir}"
 
+input_file_contents = IO.read input_file
+Parser
+    .parse(input_file_contents)
+    .each {|s|
+        output_file = File.join(output_dir, s.filename)
+        output_contents = Formatter.format s
 
+        puts "Writing file: #{output_file}"
+
+        File.write(output_file, output_contents)
+    }
+
+puts "Generation Complete!"
