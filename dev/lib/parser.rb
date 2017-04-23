9a996a5cf1bf61dbb2873d8d1deaf56a2b39c627
Camilo Sanchez <camilin87@gmail.com>
2017-04-23 01:13:17 -0400

    enhance the parse to take on multiple lines

--- dev/lib/parser.rb
+++ dev/lib/parser.rb
@@ -7,20 +7,25 @@ end
 class Parser
     def self.parse(markdown)
         markdown = markdown || ""
-        markdown = markdown.strip
 
-        m = /^\!\[(\d+)\s\-\s(.+)\]\((.+)\)$/.match(markdown)
-        if m == nil
-            return []
+        result = []
+
+        markdown.lines.each do |line|
+            line = line.strip
+
+            m = /^\!\[(\d+)\s\-\s(.+)\]\((.+)\)$/.match(line)
+            if m != nil
+                data = m.captures
+
+                r = StatusCodeInfo.new
+                r.code = data[0].to_i
+                r.description = data[1]
+                r.url = data[2]
+
+                result.push r
+            end
         end
 
-        data = m.captures
-
-        r = StatusCodeInfo.new
-        r.code = data[0].to_i
-        r.description = data[1]
-        r.url = data[2]
-
-        return [r]
+        return result
     end
 end
--- dev/spec/parser_spec.rb
+++ dev/spec/parser_spec.rb
@@ -26,4 +26,29 @@ describe Parser do
         expect(info.description).to eq "Continue"
         expect(info.url).to eq "http://i.imgur.com/mi9lcO6.jpg"
     end
+
+    it "parses an entire file" do 
+        input = %{
+###101 - Switching Protocols
+![101 - Switching Protocols](http://i.imgur.com/p767VEv.jpg)  
+
+## 2xx  
+
+###200 - OK
+![200 - OK](http://i.imgur.com/HXotKm9.jpg)  
+}
+        result = parse(input)
+
+        expect(result.length).to eq 2
+
+        expect(result[0]).to be_kind_of StatusCodeInfo
+        expect(result[0].code).to eq 101
+        expect(result[0].description).to eq "Switching Protocols"
+        expect(result[0].url).to eq "http://i.imgur.com/p767VEv.jpg"
+
+        expect(result[1]).to be_kind_of StatusCodeInfo
+        expect(result[1].code).to eq 200
+        expect(result[1].description).to eq "OK"
+        expect(result[1].url).to eq "http://i.imgur.com/HXotKm9.jpg"
+    end
 end
