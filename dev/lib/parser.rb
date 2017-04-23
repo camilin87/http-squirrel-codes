d875946e5cb1fc4c8b48712835dd94fc3f3fd680
Camilo Sanchez <camilin87@gmail.com>
2017-04-23 01:15:23 -0400

    refactor the parser a little bit

--- dev/lib/parser.rb
+++ dev/lib/parser.rb
@@ -2,30 +2,21 @@ class StatusCodeInfo
     attr_accessor :code
     attr_accessor :description
     attr_accessor :url
+
+    def initialize(code, description, url)
+        @code = code
+        @description = description
+        @url = url
+    end
 end
 
 class Parser
     def self.parse(markdown)
-        markdown = markdown || ""
-
-        result = []
-
-        markdown.lines.each do |line|
-            line = line.strip
-
-            m = /^\!\[(\d+)\s\-\s(.+)\]\((.+)\)$/.match(line)
-            if m != nil
-                data = m.captures
-
-                r = StatusCodeInfo.new
-                r.code = data[0].to_i
-                r.description = data[1]
-                r.url = data[2]
-
-                result.push r
-            end
-        end
-
-        return result
+        (markdown || "")
+            .lines
+            .map { |l| /^\!\[(\d+)\s\-\s(.+)\]\((.+)\)\s\s$/.match l }
+            .select { |m| m != nil }
+            .map { |m| m.captures }
+            .map { |data| StatusCodeInfo.new(data[0].to_i, data[1], data[2]) }
     end
 end
