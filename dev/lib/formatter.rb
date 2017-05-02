d3a5ee6a32ed3ac4e357a050243b9c23feef085d
Camilo Sanchez <camilin87@gmail.com>
2017-05-02 08:56:35 -0400

    change the template to enhance how the code description is displayed

--- dev/lib/formatter.rb
+++ dev/lib/formatter.rb
@@ -7,8 +7,11 @@ permalink: /#{status_code_info.code}
 <div class="status-page-container">
 <div>
     <img src=\"#{status_code_info.image_url}\" alt=\"#{status_code_info.full_name}\" />
-    <h1>#{status_code_info.code}</h1>
-    <h2>#{status_code_info.description}</h2>
+
+    <div id="code-description">
+        <h1>#{status_code_info.code}</h1>
+        <h2>#{status_code_info.description}</h2>
+    </div>
 </div>
 </div>
 }
--- dev/spec/formatter_spec.rb
+++ dev/spec/formatter_spec.rb
@@ -11,8 +11,11 @@ permalink: /725
 <div class="status-page-container">
 <div>
     <img src="/assets/img/code/725.png" alt="725 It works on my machine" />
-    <h1>725</h1>
-    <h2>It works on my machine</h2>
+
+    <div id="code-description">
+        <h1>725</h1>
+        <h2>It works on my machine</h2>
+    </div>
 </div>
 </div>
 }
